209cddedb70ad6e04399a2901c8aa580e6a1f0bb
hawraa <hawraaasaad99@gmail.com>
2026-08-23 01:10:36 +0300

    feat: add middlewarw

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.routers import users, tasks
 from app.core.database import Base, engine
 from app.core.logging import logger
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -13,6 +14,14 @@ async def lifespan(app: FastAPI):
     
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(tasks.router)

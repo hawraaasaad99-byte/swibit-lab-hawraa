47e562166dc5240ed4f8d4e622032be7286d1be7
hawraa <hawraaasaad99@gmail.com>
2026-09-08 01:04:33 +0300

    update client file

--- backend/app/AI/client.py
+++ backend/app/AI/client.py
@@ -1,18 +1,28 @@
+
 import os
 from openai import OpenAI
 
 client = OpenAI(
     api_key=os.getenv("LLM_API_KEY", "ollama"),
-    base_url=os.getenv("LLM_BASE_URL", "http://localhost:11434/v1"),
+    base_url=os.getenv(
+        "LLM_BASE_URL",
+        "http://localhost:11434/v1"
+    ),
 )
 
-async def call_llm(system_prompt: str, user_text: str) -> str:
+async def call_llm(
+    system_prompt: str,
+    user_text: str,
+    temperature: float = 0.1
+) -> str:
+
     response = client.chat.completions.create(
-        model="llama3",
+        model="codellama",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_text}
         ],
-        temperature=0.0
+        temperature=temperature
     )
+
     return response.choices[0].message.content
--- backend/app/AI/rag/answer.py
+++ backend/app/AI/rag/answer.py
@@ -2,37 +2,30 @@ import os
 import requests
 from app.AI.rag.retrieve import search_policies
 import ollama
+from app.AI.client import call_llm
 
-def answer_user_query(user_query: str):
+
+async def answer_user_query(user_query: str):
+
     docs = search_policies(user_query, k=3)
-    context = "\n\n".join([doc.page_content for doc in docs])
 
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
 
-
-    prompt = f"""You are an AI assistant for a company. Use the following context to answer the user's question accurately.
+    prompt = f"""Use the following context to answer the user's question accurately.
 If you don't know the answer based on the context, say that you don't know.
-
 
 Context:
 {context}
 
-Question: {user_query}
+Question:
+{user_query}
 
 Answer:"""
-    
-   
 
-    response = ollama.chat(
-        model = 'codellama',
-        messages =[
-            {'role':'user','content':prompt}
-        ],
-        options = {
-            'temperature':0.1,
-             'num_predict':256
-        }
-    )   
-       
-    
-    
-    return response['message']['content']
+    return await call_llm(
+        system_prompt="You are an AI assistant for a company.",
+        user_text=prompt,
+        temperature=0.1
+    )

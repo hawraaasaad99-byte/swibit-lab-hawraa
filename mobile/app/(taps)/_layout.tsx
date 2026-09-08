9c22831e141daf8c9d9921b3c543b6504c72d420
hawraa <hawraaasaad99@gmail.com>
2026-09-08 04:22:10 +0300

    fixing the layout prblem

---
+++ mobile/app/(taps)/_layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: useClientOnlyValue(false, true),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: 'Tasks',
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ mobile/app/(taps)/home.tsx
@@ -0,0 +1,5 @@
+import TasksScreen from '@/features/task/screens/TasksScreen';
+
+export default function HomeRoute() {
+  return <TasksScreen />;
+}
